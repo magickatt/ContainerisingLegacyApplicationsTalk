1c88bd571e14316e2b5810db2274f4bbcc4e6452
Andrew Kirkpatrick <a.kirkpatrick@outlook.com>
2019-05-20 20:11:57 -0400

    Fixing the persistent volume claim

--- 5_helm.sh
+++ 5_helm.sh
@@ -1,15 +1,29 @@
+# Initialise Helm
 helm init --history-max 200
-helm install --dry-run --debug ./helm
 
-helm install --name=5_helm ./helm
-
-
-
-
+# Install MySQL via Helm
 helm install --name mybb stable/mysql
 kubectl port-forward mysql-0 3306:3306
 MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace default mybb-mysql -o jsonpath="{.data.mysql-root-password}" | base64 --decode; echo)
 mysql -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD < database/mybb.sql
 
-
+# Install Memcache via Helm
 helm install --name mybb-memcache stable/memcached
+
+# Install MyBB via Helm
+helm install --dry-run --debug ./helm
+helm install --name=5_helm ./helm
+
+#kubectl port-forward mybb-memcache-memcached-0 11211:11211
+#echo "stats items" | ncat 127.0.0.1 11211
+
+
+
+cat > storageClass.yaml << EOF
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: local-storage
+provisioner: kubernetes.io/no-provisioner
+volumeBindingMode: WaitForFirstConsumer
+EOF

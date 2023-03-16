d560bb5831bd974be85122c38885e1cdb70ab9be
Andrew Kirkpatrick <andrew.kirkpatrick@stackadapt.com>
2023-03-16 17:58:12 -0400

    Updating MyBB version and adding download skip

--- 0_run.sh
+++ 0_run.sh
@@ -1,16 +1,23 @@
-VERSION=1.8.20
+VERSION=1.8.33
 VERSION_STRIPPED="${VERSION//./}"
 
 echo "Downloading and extracting MyBB ${VERSION}\n"
-sleep 1
+
+# Tidy up
 rm -Rf application/*
-# Live demo, better skip the internet
-#curl --output mybb.zip --progress-bar "https://resources.mybb.com/downloads/mybb_${VERSION_STRIPPED}.zip"
-cp ~/mybb.zip .
-unzip mybb.zip 'Upload/*' -d "/tmp/mybb${VERSION_STRIPPED}" -qq -o
+
+# Download MyBB (it not already downloaded)
+FILE="$TMPDIR/mybb.zip"
+if [ ! -f "$FILE" ]; then
+  curl --output "$TMPDIR/mybb.zip" --progress-bar "https://resources.mybb.com/downloads/mybb_${VERSION_STRIPPED}.zip"
+fi
+
+# Extract MyBB
+cp $FILE .
+unzip $FILE 'Upload/*' -d "/tmp/mybb${VERSION_STRIPPED}" -qq -o
 echo "(moving MyBB from /tmp to application)"
 cp -R "/tmp/mybb${VERSION_STRIPPED}/Upload/" application && rm -Rf "/tmp/mybb${VERSION_STRIPPED}"
-rm mybb.zip
+# rm $FILE
 
 echo "\nUse 'mybb.sdb' as the path to the SQLite database."
 cp database/mybb.sdb application
@@ -18,3 +25,4 @@ cp database/mybb.sdb application
 # https://www.php.net/manual/en/features.commandline.webserver.php
 echo "\nStarting PHP development web server"
 php -S localhost:9000 -t application
+
